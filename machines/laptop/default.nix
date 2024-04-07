9c4a359335f0a18ca7551cda0e21cac4bc2d07a7
veryfastman <dony357@outlook.com>
2024-04-07 04:51:07 +0000

    enabled networkmanager

--- machines/laptop/default.nix
+++ machines/laptop/default.nix
@@ -106,6 +106,8 @@ localFlake: {
     };
   };
 
+  networking.networkmanager.enable = true;
+
   services.printing.enable = true;
   services.udisks2.enable = true;
   services.upower.enable = true;
