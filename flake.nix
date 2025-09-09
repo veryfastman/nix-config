1ac9a6be8b285d96b9ebc7f479252e53c7e0eb4d
veryfastman <dony357@outlook.com>
2025-09-08 23:12:25 -0700

    replace zen-browser-flake with up-to-date version

--- flake.nix
+++ flake.nix
@@ -59,7 +59,7 @@
     };
 
     zen-browser = {
-      url = "github:MarceColl/zen-browser-flake";
+      url = "0xc000022070/zen-browser-flake";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
